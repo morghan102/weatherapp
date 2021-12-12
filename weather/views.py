8f534b4f5f6ce8111d4e389edf0533a2fcffa70a
morghan102 <morghan102@gmail.com>
2021-12-12 14:12:45 -0800

    config

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(req):
+    if req.method == 'POST':
+        city = req.POST['city']
+    else:
+        city = ''
+    return render(req, 'index.html', {'city': city})
